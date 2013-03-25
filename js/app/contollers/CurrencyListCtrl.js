870dc3e32f2b53821182ecda391426267ddd1267
tabalinas <tabalinas@gmail.com>
2013-03-26 00:06:41 +0400

    currency chart first milestone

--- js/app/contollers/CurrencyListCtrl.js
+++ js/app/contollers/CurrencyListCtrl.js
@@ -4,7 +4,7 @@
 
     app.ctrls.CurrencyListCtrl = function($scope, $http) {
         $scope.currencies = app.db.Currencies;
-        
+
         $scope.convert = function() {
             var self = this,
                 valueToConvert = self.value,
@@ -31,6 +31,61 @@
 
             $.when.apply(null, promises).done(function() {
                 self.$apply();
+                self.renderChart()
+            });
+        };
+
+        $scope.renderChart = function() {
+            var series = [];
+
+            angular.forEach(this.currencies, function(currency) {
+                series.push({
+                    name: currency.ticker,
+                    data: [currency.value]
+                });
+            });
+
+            $('#chart').highcharts({
+                chart: {
+                    type: 'bar'
+                },
+                series: series,
+                title: {
+                    text: 'Value in different currencies'
+                },
+                xAxis: {
+                    title: {
+                        text: null
+                    }
+                },
+                yAxis: {
+                    min: 0,
+                    title: {
+                        text: null
+                    },
+                    labels: {
+                        overflow: 'justify'
+                    }
+                },
+                tooltip: {
+                    formatter: function() {
+                        return "<b>" + this.series.name + "</b>: " + this.y.toFixed(2);
+                    }
+                },
+                legend: {
+                    enabled: false
+                },
+                plotOptions: {
+                    bar: {
+                        dataLabels: {
+                            enabled: true,
+                            formatter: function() {
+                                return this.y.toFixed(2);
+                            }
+                        }
+                    }
+                },
+                credits: { enabled: false }
             });
         };
     };
